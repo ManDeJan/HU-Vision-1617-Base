b09e916c64440543d20d0ac4aa816afc8950c371
ManDeJan <janhlsm@gmail.com>
2017-03-23 20:30:27 +0100

    Implemented Canny Edge detection

--- source/ExternalDLL/ExternalDLL/JanEnDavidFunctionHouse.h
+++ source/ExternalDLL/ExternalDLL/JanEnDavidFunctionHouse.h
@@ -1,9 +1,12 @@
 #pragma once
+#include "ImageIO.h"
 #include "StudentPreProcessing.h"
 #include <cmath>
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <queue>
+#include <string>
 #include <vector>
 
 double gaussian(double x, double mu, double sigma) {
@@ -68,31 +71,45 @@ IntensityImage *combineImage(const IntensityImage &image1, const IntensityImage 
     return returnImage;
 }
 
+using gradientImage = std::vector<std::vector<double>>;
+using refinedGradientImage = std::vector<std::vector<uint8_t>>;
+
 IntensityImage *kernelApplyer(const IntensityImage &image, std::vector<kernel_type> kernels,
-                              std::function<int(std::vector<int>)> merge = [](std::vector<int> a) {
+                              std::function<int(std::vector<int>)> merge =
+                              [](std::vector<int> a) {
                                   int total = 0;
                                   for (auto x : a) {
                                       total += x;
                                   }
                                   return total;
-                              }) {
+                              },
+                              gradientImage *gradientDirection = nullptr) {
     // Print kernel
 
     uint kwidth = kernels[0].size();
     uint kheight = kwidth;
-    kernelPrinter(kernels[0]);
 
-    // IntensityImagePrivate returnImage = IntensityImagePrivate(image.getWidth(), image.getHeight());
     IntensityImage *returnImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
+    if (gradientDirection) {
+        gradientDirection->resize(image.getWidth());
+        for (size_t i = 0; i < image.getWidth(); i++) {
+            (*gradientDirection)[i].resize(image.getHeight());
+        }
+    }
+
+    int ducks = 0;
+    int kernelAmount = kernels.size();
 
     for (int y = 0; y < image.getHeight(); y++) {
         for (int x = 0; x < image.getWidth(); x++) {
 
             std::vector<int> newPekels = { 0 };
             int              newPekel = 0;
-            newPekels.reserve(kernels.size());
+            newPekels.resize(kernelAmount);
             for (int i = 0; i < kheight; i++) {
                 for (int j = 0; j < kwidth; j++) {
+
+                    // bounds checking
                     int realX = x + j - kwidth / 2;
                     int realY = y + i - kheight / 2;
                     if (realX < 0) realX = 0;
@@ -100,11 +117,15 @@ IntensityImage *kernelApplyer(const IntensityImage &image, std::vector<kernel_ty
                     if (realY < 0) realY = 0;
                     if (realY > image.getHeight()) realY = image.getHeight();
 
-                    for (size_t k = 0; k < kernels.size(); k++) {
+                    for (size_t k = 0; k < kernelAmount; k++) {
                         newPekels[k] += image.getPixel(realX, realY) * kernels[k][i][j];
                     }
-                    // std::cout << realX << ' ' << realY << '\n';
                 }
+            }
+            if (gradientDirection) {
+                double direction = (atan2(newPekels[0], newPekels[1]) * 180 / M_PI);
+                if (direction < 0) direction += 180;
+                (*gradientDirection)[x][y] = direction;
             }
             newPekel = merge(newPekels);
 
@@ -112,6 +133,118 @@ IntensityImage *kernelApplyer(const IntensityImage &image, std::vector<kernel_ty
             if (newPekel < 0) newPekel = 0;
 
             returnImage->setPixel(x, y, (uint8_t)newPekel);
+        }
+    }
+
+    return returnImage;
+}
+
+refinedGradientImage gradientRefiner(gradientImage &x) {
+    refinedGradientImage image;
+
+    image.resize(x.size());
+    for (size_t i = 0; i < x.size(); i++) {
+        image[i].resize(x[0].size());
+    }
+
+    for (size_t i = 0; i < x.size(); i++) {
+        for (size_t j = 0; j < x[i].size(); j++) {
+            double  direction = x[i][j];
+            uint8_t refined = 0;
+            if (direction > 22.5 && direction < 67.5) {
+                refined = 45;
+            } else if (direction > 67.5 && direction < 112.5) {
+                refined = 90;
+            } else if (direction > 112.5 && direction < 157.5) {
+                refined = 135;
+            }
+            image[i][j] = refined;
+        }
+    }
+
+    return image;
+}
+
+IntensityImage *imageSurpressor(const IntensityImage &image, const refinedGradientImage &hoekies) {
+    IntensityImage *returnImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
+    for (size_t i = 0; i < image.getHeight() * image.getWidth(); i++) {
+        returnImage->setPixel(i, image.getPixel(i));
+    }
+
+    std::cout << "sizeof image en hoekies ding: " << hoekies.size() << ' ' << image.getWidth() << '\n';
+    std::cout << "sizeof image en hoekies ding: " << hoekies[0].size() << ' ' << image.getHeight() << '\n';
+
+    for (uint i = 0; i < image.getHeight(); i++)
+        for (uint j = 0; j < image.getWidth(); j++) {
+            int ax, ay, bx, by;
+            int hoek = hoekies[j][i];
+            if (hoek == 0) {
+                ax = j + 1;
+                ay = i;
+                bx = j - 1;
+                by = i;
+            } else if (hoek == 45) {
+                ax = j + 1;
+                ay = i + 1;
+                bx = j - 1;
+                by = i - 1;
+            } else if (hoek == 90) {
+                ax = j;
+                ay = i + 1;
+                bx = j;
+                by = i - 1;
+            } else /* if (hoek == 135) */ {
+                ax = j - 1;
+                ay = i + 1;
+                bx = j + 1;
+                by = i - 1;
+            }
+
+            if (ax < 0 || ax >= image.getWidth() || ay < 0 || ay >= image.getHeight())
+                continue;
+            else if (image.getPixel(ax, ay) > image.getPixel(j, i)) {
+                returnImage->setPixel(j, i, 0);
+                continue;
+            }
+
+            if (bx < 0 || bx >= image.getWidth() || by < 0 || by >= image.getHeight())
+                continue;
+            else if (image.getPixel(bx, by) > image.getPixel(j, i)) {
+                returnImage->setPixel(j, i, 0);
+                continue;
+            }
+        }
+    return returnImage;
+}
+
+IntensityImage *hysterisch(const IntensityImage &image, int thresholdLow, int thresholdHigh) {
+    using ints = std::pair<int, int>;
+    IntensityImage * returnImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
+    std::queue<ints> nodes;
+
+    for (size_t i = 0; i < image.getHeight(); i++) {
+        for (size_t j = 0; j < image.getWidth(); j++) {
+            if ((image.getPixel(j, i) >= thresholdHigh) && (returnImage->getPixel(j, i) != 255)) {
+                nodes.push(ints{ j, i });
+                while (!nodes.empty()) {
+                    ints node = nodes.front();
+                    nodes.pop();
+                    int x = node.first, y = node.second;
+                    if (x < 0 || x >= image.getWidth() || y < 0 || y >= image.getHeight()) continue;
+                    if (image.getPixel(x, y) < thresholdLow) continue;
+                    if (returnImage->getPixel(x, y) != 255) {
+                        returnImage->setPixel(x, y, 255);
+                        nodes.push(ints{x+1,y-1});
+                        nodes.push(ints{x+1,y  });
+                        nodes.push(ints{x+1,y+1});
+                        nodes.push(ints{x  ,y+1});
+                        nodes.push(ints{x  ,y-1});
+                        nodes.push(ints{x-1,y-1});
+                        nodes.push(ints{x-1,y  });
+                        nodes.push(ints{x-1,y+1});
+                    }
+                }
+            }
         }
     }
 
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -15,8 +15,8 @@ IntensityImage *StudentPreProcessing::stepScaleImage(const IntensityImage &image
 // Here we do edgy detection.
 IntensityImage *StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
     // First we blur.
-    int radius = 2;
-    double sigma  = 1;
+    int radius = 1;
+    double sigma  = 0.5;
 
     kernel_type sobel_X = {{-1,0,1},
                            {-2,0,2},
@@ -25,6 +25,11 @@ IntensityImage *StudentPreProcessing::stepEdgeDetection(const IntensityImage &im
     kernel_type sobel_Y = {{-1,-2,-1},
                            { 0, 0, 0},
                            { 1, 2, 1}};
+
+    kernel_type fopKernel = {{ 0, 0, 0},
+                             { 0, 1, 0},
+                             { 0, 0, 0}};
+
 
     kernel_type laplacian = {{0.5 ,1, 0.5},
                              {1,  -6,   1},
@@ -38,13 +43,45 @@ IntensityImage *StudentPreProcessing::stepEdgeDetection(const IntensityImage &im
         return sqrt(pow(pekkels[0], 2) + pow(pekkels[1], 2));
     };
 
-    IntensityImage* sobelImage = kernelApplyer(*blurImage, {sobel_X, sobel_Y}, sobel_merge);
+    auto fop_merge = [](std::vector<int> pekkels) {
+        return pekkels[1];
+    };
+
+    gradientImage gradientDirection;
+    IntensityImage* sobelImage = kernelApplyer(*blurImage, {sobel_X, sobel_Y}, sobel_merge, &gradientDirection);
+
+    // kernelPrinter(gradientDirection);
+
+    // for (size_t i = 0; i < sobelImage->getHeight(); i++) {
+    //     for (size_t j = 0; j < sobelImage->getWidth(); j++) {
+    //         if (gradientDirection[j][i] > 0) {
+    //             sobelImage->setPixel(j, i, 255);
+    //         } else {
+    //             sobelImage->setPixel(j, i, 0);
+    //         }
+    //
+    //     }
+    // }
 
     // IntensityImage* lapLacian = kernelApplyer(*blurImage, laplacian);
 
-    return sobelImage;
+    refinedGradientImage refinedGradientDirection = gradientRefiner(gradientDirection);
+
+    IntensityImage* surpressedImage = imageSurpressor(*sobelImage, refinedGradientDirection);
+
+    IntensityImage* hystericImage = hysterisch(*surpressedImage, 5, 110);
+
+
+
+
+    return hystericImage;
 }
 
 IntensityImage *StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
-    return nullptr;
+    IntensityImage *returnImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());;
+    for (size_t i = 0; i < image.getHeight() * image.getWidth(); i++) {
+        returnImage->setPixel(i, image.getPixel(i));
+    }
+    std::cout << "Klaar met onze kak <3" << '\n';
+    return returnImage;
 }
